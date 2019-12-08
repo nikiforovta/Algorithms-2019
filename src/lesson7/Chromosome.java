604538eb786075cd9b5c4724c63bfa3d4fee6f71
Timofey <nikiforov.ta@edu.spbstu.ru>
2019-12-09 01:04:09 +0300

    Lesson 7

---
+++ src/lesson7/Chromosome.java
@@ -0,0 +1,106 @@
+package lesson7;
+
+import lesson6.knapsack.Item;
+
+import java.util.List;
+import java.util.Random;
+
+public class Chromosome {
+
+    private String genome = "";
+    private int fitness;
+
+    public String getGenome() {
+        return genome;
+    }
+
+    public void setGenome(String genome) {
+        this.genome = genome;
+    }
+
+    public int getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(int fitness) {
+        this.fitness = fitness;
+    }
+
+    public static void createChromosome(Chromosome chromosome, int genesCount) {
+        Random random = new Random();
+        StringBuilder resultGenome = new StringBuilder();
+
+        for (int i = 0; i < genesCount; i++) {
+            resultGenome.append(' ');
+            resultGenome.setCharAt(i, random.nextInt(2) == 0 ? '0' : '1');
+        }
+        chromosome.setGenome(resultGenome.toString());
+    }
+
+    public Item getFilledKnapsack(List<Item> items) {
+        int costSum = 0;
+        int weightSum = 0;
+        for (int i = 0; i < items.size(); i++) {
+            if (this.genome.charAt(i) == '1') {
+                costSum += items.get(i).getCost();
+                weightSum += items.get(i).getWeight();
+            }
+        }
+        return new Item(costSum, weightSum);
+    }
+
+    public int calculateFitness(List<Item> items, int load) {
+        Item filledKnapsack = getFilledKnapsack(items);
+        if (filledKnapsack.getWeight() > load) {
+            return 0;
+        }
+        return filledKnapsack.getCost();
+    }
+
+    @Override
+    protected Object clone() {
+        Chromosome resultChromosome = new Chromosome();
+        resultChromosome.setFitness(this.getFitness());
+        resultChromosome.setGenome(genome);
+        return resultChromosome;
+    }
+
+    public Chromosome mutation(int genesCount, int generation) {
+        Chromosome result = (Chromosome) this.clone();
+        StringBuilder resultGenome = new StringBuilder(genome);
+
+        for (int i = 0; i < genesCount; i += generation) {
+            double randomPercent = Math.random() * 100;
+            if (randomPercent < 5) {
+                char oldValue = genome.charAt(i);
+                char newValue = oldValue == '1' ? '0' : '1';
+                resultGenome.setCharAt(i, newValue);
+            }
+        }
+        result.setGenome(resultGenome.toString());
+        return result;
+    }
+
+    public String toString() {
+        return "Genome: " + genome + "\n" +
+                "Fitness: " + fitness + "\n";
+    }
+
+    public Chromosome crossover(Chromosome chromosome, int genesCount) {
+        Chromosome result = new Chromosome();
+        Random random = new Random();
+        StringBuilder resultGenome = new StringBuilder();
+
+        for (int i = 0; i < genesCount; i++) {
+            resultGenome.append(' ');
+            int xor = this.getGenome().charAt(i) ^ chromosome.getGenome().charAt(i);
+            if (xor == 0) {
+                resultGenome.setCharAt(i, this.getGenome().charAt(i));
+            } else {
+                resultGenome.setCharAt(i, (char) (random.nextInt(2) + '0'));
+            }
+        }
+        result.setGenome(resultGenome.toString());
+        return result;
+    }
+}
--- src/lesson7/JavaHeuristicsTasks.java
+++ src/lesson7/JavaHeuristicsTasks.java
@@ -7,6 +7,7 @@ import lesson6.knapsack.Fill;
 import lesson6.knapsack.Item;
 
 import java.util.List;
+import java.util.Random;
 
 // Примечание: в этом уроке достаточно решить одну задачу
 @SuppressWarnings("unused")
@@ -14,27 +15,76 @@ public class JavaHeuristicsTasks {
 
     /**
      * Решить задачу о ранце (см. урок 6) любым эвристическим методом
-     *
+     * <p>
      * Очень сложная
-     *
+     * <p>
      * load - общая вместимость ранца, items - список предметов
-     *
+     * <p>
      * Используйте parameters для передачи дополнительных параметров алгоритма
      * (не забудьте изменить тесты так, чтобы они передавали эти параметры)
      */
-    public static Fill fillKnapsackHeuristics(int load, List<Item> items, Object... parameters) {
-        throw new NotImplementedError();
+
+    static final int POPULATION = 100;
+    static final int GENERATIONS = 1500;
+    static Chromosome[] population = new Chromosome[POPULATION];
+
+    public static Fill fillKnapsackHeuristics(int load, List<Item> items) {
+        int genesCount = items.size();
+        for (int i = 0; i < POPULATION; i++) {
+            population[i] = new Chromosome();
+            Chromosome.createChromosome(population[i], genesCount);
+        }
+        for (int i = 0; i < GENERATIONS; i++) {
+            for (int j = 0; j < POPULATION; j++) {
+                population[j].setFitness(population[j].calculateFitness(items, load));
+            }
+            population = createNextGeneration(genesCount, i + 1);
+        }
+        if ((population[0].getFitness()) == 0) return new Fill(0);
+        return new Fill(population[0].getFilledKnapsack(items));
+    }
+
+    private static Chromosome[] createNextGeneration(int genesCount, int generation) {
+        Chromosome[] nextGeneration = new Chromosome[POPULATION];
+        Random random = new Random();
+        nextGeneration[0] = findBestChromosome();
+        for (int i = 1; i < POPULATION; i++) {
+            if (population[i].getFitness() == 0) {
+                Chromosome firstParent = population[random.nextInt(POPULATION)];
+                Chromosome secondParent = population[random.nextInt(POPULATION)];
+                while (secondParent == firstParent) {
+                    secondParent = population[random.nextInt(POPULATION)];
+                }
+                Chromosome result = firstParent.crossover(secondParent, genesCount);
+                nextGeneration[i] = result.mutation(genesCount, generation);
+            } else {
+                nextGeneration[i] = population[i];
+            }
+        }
+        return nextGeneration;
+    }
+
+    private static Chromosome findBestChromosome() {
+        int bestFitness = 0;
+        Chromosome result = population[0];
+        for (int i = 0; i < POPULATION; i++) {
+            if (population[i].getFitness() > bestFitness) {
+                result = population[i];
+                bestFitness = population[i].getFitness();
+            }
+        }
+        return result;
     }
 
     /**
      * Решить задачу коммивояжёра (см. урок 5) методом колонии муравьёв
      * или любым другим эвристическим методом, кроме генетического и имитации отжига
      * (этими двумя методами задача уже решена в под-пакетах annealing & genetic).
-     *
+     * <p>
      * Очень сложная
-     *
+     * <p>
      * Граф передаётся через параметр graph
-     *
+     * <p>
      * Используйте parameters для передачи дополнительных параметров алгоритма
      * (не забудьте изменить тесты так, чтобы они передавали эти параметры)
      */
